53d9a05adff394a70f6a2174d46477b8c96e42a0
Sarvesh Kulkarni <kulkarni.2002sarvesh@gmail.com>
2024-01-21 12:12:08 +0530

    my laptop changes

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,7 @@
 import React from "react";
+import server from "./server"
 function Row ({title}){
+    
     return(
         <div>
             <h2>{title}</h2>
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
-const instance =axios.create({
+const server  = axios.create({
     baseURL:"https://api.themoviedb.org/3",
     
 });
 
-export default instance;
+export default server;
