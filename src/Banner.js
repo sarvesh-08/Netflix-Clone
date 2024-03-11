e71fed7e9fa2753417593f0bdbf056f478098d32
Sarvesh Kulkarni <sarvesh_kulkarni@persistent.com>
2024-03-11 11:22:52 +0530

    installed gh-pages and added scripts for predeploy and deploy

--- src/Banner.js
+++ src/Banner.js
@@ -19,7 +19,7 @@ function Banner() {
       return movie;
     }
     fetchData();
-  },[]);
+  }, []);
   console.log(movie);
   function truncate(str, n) {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
